91c3c5f8867ded1f906279f5c4d3cacd0a94b177
CMcDonnell12 <113586649+CMcDonnell12@users.noreply.github.com>
2022-09-15 09:46:30 +0100

    Create turtlePetal.py

---
+++ turtlePetal.py
@@ -0,0 +1,11 @@
+# turtlePetal.py
+# @CMcDonnell 15/09/2022
+
+from turtle import *
+import random
+shape('turtle')
+color('black')
+for i in range(4):
+    circle(50,90)
+    left(90)
+    circle(50,90)
