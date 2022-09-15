a4ec41a44887b4a56f146b3f3f2ffa901a5ad182
CMcDonnell12 <113586649+CMcDonnell12@users.noreply.github.com>
2022-09-15 09:17:41 +0100

    Create turtleFilledCircles.py

---
+++ turtleFilledCircles.py
@@ -0,0 +1,14 @@
+#turtleFilledCircles.py
+#@CMcDonnell 
+
+from turtle import *
+shape('turtle')
+color('purple')
+begin_fill()
+circle(50)
+end_fill()
+left(90)
+color('blue')
+begin_fill()
+circle(50)
+end_fill()
